18d1042e249e164b0b99baee79f56d3de2ea0820
Johannes Krieg <johannes.krieg@outlook.de>
2019-02-18 20:57:02 +0100

    -

---
+++ Commonfunctions/Cryptography.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Commonfunctions.Cryptography
+{
+    public static class MD5
+    {
+        /// <summary>
+        /// Gibt einen MD5 Hash als String zurück
+        /// </summary>
+        /// <param name="TextToHash">string der Gehasht werden soll.</param>
+        /// <returns>Hash als string.</returns>
+        public static string GetMD5Hash(string TextToHash)
+        {
+            //Prüfen ob Daten übergeben wurden.
+            if ((TextToHash == null) || (String.IsNullOrEmpty(TextToHash)))
+            {
+                return string.Empty;
+            }
+
+            //MD5 Hash aus dem String berechnen. Dazu muss der string in ein Byte[]
+            //zerlegt werden. Danach muss das Resultat wieder zurück in ein string.
+            System.Security.Cryptography.MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] textToHash = Encoding.Default.GetBytes(TextToHash);
+            byte[] result = md5.ComputeHash(textToHash);
+
+            return System.BitConverter.ToString(result);
+        }
+    }
+
+    public static class SimpleAES
+    {
+        /// <summary>
+        /// Encrypts the string.
+        /// </summary>
+        /// <param name="clearText">The clear text.</param>
+        /// <param name="Key">The key.</param>
+        /// <param name="IV">The IV.</param>
+        /// <returns></returns>
+        private static byte[] EncryptString(byte[] clearText, byte[] Key, byte[] IV)
+        {
+            MemoryStream ms = new MemoryStream();
+            Rijndael alg = Rijndael.Create();
+            alg.Key = Key;
+            alg.IV = IV;
+            CryptoStream cs = new CryptoStream(ms, alg.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(clearText, 0, clearText.Length);
+            cs.Close();
+            byte[] encryptedData = ms.ToArray();
+            return encryptedData;
+        }
+
+        /// <summary>
+        /// Encrypts the string.
+        /// </summary>
+        /// <param name="clearText">The clear text.</param>
+        /// <param name="Password">The password.</param>
+        /// <returns></returns>
+        public static string EncryptString(string clearText, string Password)
+        {
+            return Convert.ToBase64String(EncryptString(System.Text.Encoding.Unicode.GetBytes(clearText), Password));
+        }
+
+        /// <summary>
+        /// Encrypts the string.
+        /// </summary>
+        /// <param name="clearText">The clear text as byte array</param>
+        /// <param name="Password">The password.</param>
+        /// <returns></returns>
+        public static byte[] EncryptString(byte[] clearText, string Password)
+        {
+            byte[] clearBytes = clearText;
+            PasswordDeriveBytes pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+            byte[] encryptedData = EncryptString(clearBytes, pdb.GetBytes(32), pdb.GetBytes(16));
+            return encryptedData;
+        }
+
+        /// <summary>
+        /// Decrypts the string.
+        /// </summary>
+        /// <param name="cipherData">The cipher data.</param>
+        /// <param name="Key">The key.</param>
+        /// <param name="IV">The IV.</param>
+        /// <returns></returns>
+        private static byte[] DecryptString(byte[] cipherData, byte[] Key, byte[] IV)
+        {
+            MemoryStream ms = new MemoryStream();
+            Rijndael alg = Rijndael.Create();
+            alg.Key = Key;
+            alg.IV = IV;
+            CryptoStream cs = new CryptoStream(ms, alg.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(cipherData, 0, cipherData.Length);
+            cs.Close();
+            byte[] decryptedData = ms.ToArray();
+            return decryptedData;
+        }
+
+        /// <summary>
+        /// Decrypts the string.
+        /// </summary>
+        /// <param name="cipherText">The cipher text.</param>
+        /// <param name="Password">The password.</param>
+        /// <returns></returns>
+        public static string DecryptString(string cipherText, string Password)
+        {
+            return System.Text.Encoding.Unicode.GetString(DecryptString(Convert.FromBase64String(cipherText), Password));
+        }
+
+        /// <summary>
+        /// Decrypts the string.
+        /// </summary>
+        /// <param name="cipherText">The cipher text as byte array</param>
+        /// <param name="Password">The password.</param>
+        /// <returns></returns>
+        public static byte[] DecryptString(byte[] cipherText, string Password)
+        {
+            byte[] cipherBytes = cipherText;
+            PasswordDeriveBytes pdb = new PasswordDeriveBytes(Password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+            byte[] decryptedData = DecryptString(cipherBytes, pdb.GetBytes(32), pdb.GetBytes(16));
+            return decryptedData;
+        }
+    }
+}
